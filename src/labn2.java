7b22b411241545f3a284106f3949b51c3cbef66b
poriesto <poriesto@hotmail.com>
2014-02-22 21:44:16 +0400

    reworked get methods

--- src/labn2.java
+++ src/labn2.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Method;
+
 /**
  * Created by poriesto on 2/14/14.
 2. Создать приложение с 3 классами- А, B , С и 2 интерфейсами I1, I2.
@@ -12,35 +14,38 @@
 */
 interface Interface1{
     String name = "Interface 1";
-    String i1();
+    String i1() throws NoSuchMethodException;
 }
 interface Interface2 extends Interface1{
     String i2();
 }
 class A{
+    public void i1(int itr){
+
+    }
     public String a1(){
         Class c = this.getClass();
         return c.getName();
     }
 }
 class B extends A implements Interface2{
+    Class c = B.class;
+    Class[] interfaces = c.getInterfaces();
+    Method[] methods = c.getMethods();
+
     public String i1(){
         String str = " ";
-        Class c = B.class;
-        Class[] interfaces = c.getInterfaces();
-        for(Class cInterface : interfaces){
-            str += (cInterface.getName() + " ");
-        }
-        return str;
+        return str + " Method: " + methods[3].getName();
     }
     public String i2(){
         String str = " ";
-        Class c = B.class;
-        Class[] interfaces = c.getInterfaces();
+        String str1 = " ";
+
         for(Class cInterface : interfaces){
             str += (cInterface.getName() + " ");
         }
-        return str;
+
+        return str + " Method: " + methods[1].getName();
     }
     public String b1(){
         Class c = this.getClass();
@@ -48,14 +53,16 @@ class B extends A implements Interface2{
     }
 }
 class C extends A implements Interface1{
+    Class c = C.class;
+    Class[] interfaces = c.getInterfaces();
+    Method[] methods = c.getMethods();
+
     public String i1(){
-        Class c = C.class;
-        Class[] interfaces = c.getInterfaces();
         String str = " ";
         for(Class cInterface : interfaces){
             str += (cInterface.getName() + " ");
         }
-        return str;
+        return str + " Method: " + methods[0].getName();
     }
     public String c1(){
         Class c = this.getClass();
@@ -69,8 +76,10 @@ public class labn2 {
         A a = new A();
         B b = new B();
         C c = new C();
-        System.out.println("Name of interface: " + " Name of class: " + a.a1());
-        System.out.println("Name of interface:" + b.i2() + " Name of class: " + b.b1());
-        System.out.println("Name of interface:" + c.i1() + " Name of class: " + c.c1());
+        //System.out.println("Name of interface: " + " Name of class: " + a.a1());
+        //System.out.println("Name of interface:" + b.i2() + " Name of class: " + b.b1());
+        //System.out.println("Name of interface:" + c.i1() + " Name of class: " + c.c1());
+        System.out.println("\nTesting of class B:\n" + "Method i1: " + b.i1() + "\nMethod b1: " + b.b1() + "\nMethod i2: " + b.i2());
+        System.out.println("\nTesting of class C:\n" + "Method i1: " + c.i1() + "\nMethod c1: " + c.c1());
     }
 }
